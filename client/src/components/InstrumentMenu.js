3b7a927e130e9f0ad182800bee2310b81c4eda81
Katy Currie <katylcurrie@gmail.com>
2021-05-01 19:31:02 +0100

    Integrated images into clickable instrument menu

--- client/src/components/InstrumentMenu.js
+++ client/src/components/InstrumentMenu.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Trumpet from "./Trumpet.png";
+import Double_Bass from "./Double_Bass.png";
+import Electric_Guitar from "./Electric_Guitar.png";
+import Saxophone from "./Saxophone.png";
+
 
 
 
@@ -9,9 +14,13 @@ const InstrumentMenu = ({instrumentList, onClick}) => {
     //     console.log(event.target.value);
     // }
 
+    const menu_images = {"Trumpet":Trumpet, "Double Bass":Double_Bass, "Electric Guitar":Electric_Guitar, "Saxophone":Saxophone}
+
     const instrumentsItems = instrumentList.map((instrument, index) => {
         return (
-        <div id='instrument-select' onClick={onClick} index={index} key={index}>{instrumentList[index].name}</div>
+        // <div id='instrument-select' onClick={onClick} index={index} key={index} src={Trumpet} alt="" >{instrumentList[index].name} </div>
+        // <img id='instrument-select' onClick={onClick} index={index} key={index} type="image" src={Trumpet} alt="" />
+        <img id='instrument-select' onClick={onClick} index={index} key={index} type="image" src={menu_images[instrument.name]} alt="" />
         );
     })
 
