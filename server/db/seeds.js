e4deaa0ba9e468b4432a26941284baac528cde61
Andrew Sharkey <sharkand@hotmail.com>
2021-05-01 18:02:39 +0100

    Added sax info to seeds.js

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -9,18 +9,28 @@ db.instruments.insertMany([
     sound: "",
     //slides
     slides: [
-    ["I am the first paragraph of the first trumpet slide", "I am the second paragraph of the first trumpet slide", "I am the third paragraph of the first trumpet slide"],
-    ["I am the first paragraph of the second trumpet slide", "I am the second paragraph of the second trumpet slide", "I am the third paragraph of the second trumpet slide"],
-    ["I am the first paragraph of the third trumpet slide", "I am the second paragraph of the third trumpet slide", "I am the third paragraph of the third trumpet slide"]
+            [
+            "The saxophone is a a type of woodwind instrument.",
+            "It is normally made of brass and consists of a mouthpiece, 20-23 keys, and a bell.",
+            "The mouthpiece has a reed which vibrates to produce the sound."
+            ],
+            [
+            "It was invented in Belgium in 1846 by Adolphe Sax.  He based its design on that of the bass clarinet.",
+            "There are four common types of saxophone: tenor, alto, baritone and soprano, plus some less common ones such as the subcontrabass sax, which can be up to 9 feet (2.75m) tall!"
+            ],
+            [
+            "The saxophone is associated with many styles of music, including classical, marching bands, blues, pop and rock, but is probably most commonly associated with jazz.",
+            "Famous saxophonists from the jazz era include Charlie Parker, John Coltrane, and Cannonball Adderly."
+            ]
     ],
     //quiz
     quiz: [
-    {question: "Who invented the trumpet?", 
-    answers: [{answer: "Adolphe Trumpet", status: true}, {answer: "Adolf Hitler", status: false}, {answer: "Goldman Sax", status: false}, {answer: "Bob Holness", status: false}]},
-    {question: "What is the trumpet made out of?", 
-    answers: [{answer: "brass", status: true}, {answer: "wood", status: false}, {answer: "gold", status: false}, {answer: "plastic", status: false}]},
-    {question: "Which is the sound of the trumpet?", 
-    answers: [{answer: "1", status: true}, {answer: "2", status: false}, {answer: "3", status: false}, {answer: "4", status: false}]}
+    {question: "How many keys does a sax have?", 
+    answers: [{answer: "20-23", status: true}, {answer: "12", status: false}, {answer: "25", status: false}, {answer: "10", status: false}]},
+    {question: "Which of these is not a type of sax?", 
+    answers: [{answer: "Goldman Sax", status: true}, {answer: "Alto Sax", status: false}, {answer: "Tenor Sax", status: false}, {answer: "Baritone Sax", status: false}]},
+    {question: "Who invented the saxophone?", 
+    answers: [{answer: "Adolphe Sax", status: true}, {answer: "Adolf Hitler", status: false}, {answer: "Hessian Sax", status: false}, {answer: "Saxo Salt", status: false}]}
     ]
     },
 
@@ -70,21 +80,32 @@ db.instruments.insertMany([
     {name: "Saxophone",
     image: "",
     sound: "",
-    //slides
-    slides: [
-    ["I am the first paragraph of the first saxophone slide", "I am the second paragraph of the first saxophone slide", "I am the third paragraph of the first saxophone slide"],
-    ["I am the first paragraph of the second saxophone slide", "I am the second paragraph of the second saxophone slide", "I am the third paragraph of the second saxophone slide"],
-    ["I am the first paragraph of the third saxophone slide", "I am the second paragraph of the third saxophone slide", "I am the third paragraph of the third saxophone slide"]
+ //slides
+ slides: [
+    [
+    "The saxophone is a a type of woodwind instrument.",
+    "It is normally made of brass and consists of a mouthpiece, 20-23 keys, and a bell.",
+    "The mouthpiece has a reed which vibrates to produce the sound."
     ],
-    //quiz
-    quiz: [
-    {question: "Who invented the saxophone?", 
-    answers: [{answer: "Adolphe Jobbie", status: false}, {answer: "Adolf Hitler", status: false}, {answer: "Goldman Sax", status: false}, {answer: "Adolphe Sax", status: true}]},
-    {question: "What is the trumpet made out of?", 
-    answers: [{answer: "plastic", status: false}, {answer: "wood", status: false}, {answer: "gold", status: false}, {answer: "brass", status: true}]},
-    {question: "Which is the sound of the saxophone?", 
-    answers: [{answer: "1", status: false}, {answer: "2", status: false}, {answer: "3", status: false}, {answer: "4", status: true}]}
+    [
+    "It was invented in Belgium in 1846 by Adolphe Sax.  He based its design on that of the bass clarinet.",
+    "There are four common types of saxophone: tenor, alto, baritone and soprano, plus some less common ones such as the subcontrabass sax, which can be up to 9 feet (2.75m) tall!"
+    ],
+    [
+    "The saxophone is associated with many styles of music, including classical, marching bands, blues, pop and rock, but is probably most commonly associated with jazz.",
+    "Famous saxophonists from the jazz era include Charlie Parker, John Coltrane, and Cannonball Adderly."
     ]
-    }
+],
+//quiz
+quiz: [
+{question: "How many keys does a sax have?", 
+answers: [{answer: "20-23", status: true}, {answer: "12", status: false}, {answer: "25", status: false}, {answer: "10", status: false}]},
+{question: "Which of these is not a type of sax?", 
+answers: [{answer: "Goldman Sax", status: true}, {answer: "Alto Sax", status: false}, {answer: "Tenor Sax", status: false}, {answer: "Baritone Sax", status: false}]},
+{question: "Who invented the saxophone?", 
+answers: [{answer: "Adolphe Sax", status: true}, {answer: "Adolf Hitler", status: false}, {answer: "Hessian Sax", status: false}, {answer: "Saxo Salt", status: false}]}
+]
+}
+
 
   ]);
