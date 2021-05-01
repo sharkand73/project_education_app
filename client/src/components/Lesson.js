18eaec15eb665a24b5ef341fe110ac012c01d189
Gabriel Neil <gabrielneil@msn.com>
2021-05-01 14:42:56 +0100

    component file structure ready

--- client/src/components/Lesson.js
+++ client/src/components/Lesson.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Slides from './Slides.js';
+import Quiz from './Quiz.js';
 
 const Lesson = ({selectedInstrument}) => {
     return (
         <>
         <p>This is a lesson</p>
+        <Slides />
+        <Quiz />
         </>
     )
 }
