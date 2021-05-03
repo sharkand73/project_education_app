03cb5cf339fac02e6d600072cf878563df5ecdff
Katy Currie <katylcurrie@gmail.com>
2021-05-03 15:15:33 +0100

    CSS experimenting part 3

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -14,9 +14,13 @@ const Welcome = () => {
         </div>
         <div id='select-text-grid'>
                 <h1>Select a box to start a lesson</h1>
-                <h2>Welcome to Learning Box!</h2>
+        </div>
+        <div id='welcome-text-grid'  >
+            <div id='welcome-grid-container'>
+                <h2 id="welcome-slide">Welcome to Learning Box!</h2>
                 <img id='boxy-size' src={Boxy_Hello} alt=""/>
             </div>
+        </div>
         </>
     )
 }
